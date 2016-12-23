5bc43d84259e63b1f23070775549068642d309c2
Gino Bustelo <gino@bustelos.com>
2016-12-22 22:02:19 -0600

    Setting RTD theme

--- conf.py
+++ conf.py
@@ -20,6 +20,12 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# -- RTD Theme Install ----------------------------------------------------
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- General configuration ------------------------------------------------
 
@@ -82,7 +88,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -151,6 +157,3 @@ texinfo_documents = [
      author, 'Ciudadela1362', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
